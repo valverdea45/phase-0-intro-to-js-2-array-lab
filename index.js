be52b8190c80013468c59828f794b18458c56f07
Adrian <adrianvalverde45@gmail.com>
2022-09-26 14:26:55 -0400

    commit tree 6268bdf66bf951f70430a8a61135b705f0bfa154

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+let cats = [`Milo`,`Otis`, `Garfield`];
+
+function destructivelyAppendCat(){
+    cats.push(`Ralph`)
+};
+
+function destructivelyPrependCat(){
+    cats.unshift(`Bob`)
+};
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat (){
+    const allCats = [...cats, `Broom`];
+    return allCats
+}
+
+function prependCat(){
+    const newCats = [`Arnold`, ...cats]
+    return newCats
+}
+
+function removeLastCat() {
+    let cuttingCats = cats.slice(0,-1);
+    return cuttingCats;
+};
+
+function removeFirstCat(){
+    let happyCats = cats.slice(1)
+    return happyCats
+}
